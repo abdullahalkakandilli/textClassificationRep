7ae82b6d120c95e0e8a50eb83229b619096f771e
Abdullah Alka Kandilli <alkakandilli@gmail.com>
2023-02-20 23:35:36 +0100

    sixth

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,13 @@ with c2:
     st.caption("")
     st.title("Upload CSV file")
 
+
+
+with c2:
+
+    st.caption("")
+    st.title("Zero-Shot Text Classifier")
+
 with c2:
 
     uploaded_file = st.file_uploader(
@@ -51,19 +58,6 @@ with c2:
 
         st.stop()
 
-with c2:
-
-    st.caption("")
-    st.title("Zero-Shot Text Classifier")
-
-
-# We need to set up session state via st.session_state so that app interactions don't reset the app.
-
-if not "valid_inputs_received" in st.session_state:
-    st.session_state["valid_inputs_received"] = False
-
-
-############ SIDEBAR CONTENT ############
 
 st.sidebar.write("")
 
@@ -85,6 +79,30 @@ headers = {"Authorization": f"Bearer {API_KEY}"}
 
 
 st.sidebar.markdown("---")
+
+
+uploaded_file = st.file_uploader(
+    "",
+    key="1",
+    help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
+)
+
+if uploaded_file is not None:
+    file_container = st.expander("Check your uploaded .csv")
+    shows = pd.read_csv(uploaded_file)
+    uploaded_file.seek(0)
+    file_container.write(shows)
+
+else:
+    st.info(
+        f"""
+            👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
+            """
+    )
+
+    st.stop()
+
+
 
 with st.form(key="my_form"):
     labels_from_st_tags = st_tags(
