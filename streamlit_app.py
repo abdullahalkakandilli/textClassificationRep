95b8ae4b00291c5c09be92570568e77ddf155a47
Abdullah Alka Kandilli <alkakandilli@gmail.com>
2023-02-21 00:02:15 +0100

    next

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,28 +38,6 @@ with c2:
     st.title("Zero-Shot Text Classifier")
 
 
-with c2:
-
-    uploaded_file = st.file_uploader(
-        "",
-        key="1",
-        help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
-    )
-
-    if uploaded_file is not None:
-        file_container = st.expander("Check your uploaded .csv")
-        shows = pd.read_csv(uploaded_file)
-        uploaded_file.seek(0)
-        file_container.write(shows)
-
-    else:
-        st.info(
-            f"""
-                👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
-                """
-        )
-
-
 
 # We need to set up session state via st.session_state so that app interactions don't reset the app.
 
